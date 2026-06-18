97ace8a43237bf918ddf096fc269d5f84cbd1e20
Eduardo <eduardo.l.oliveira12@aluno.senai.br>
2026-06-18 09:53:01 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { View, Text,FlatList, StyleSheet, Image } from "react-native";
 
 export default function App() {
+
+  const [characters, setCharacters] = useState([]);
+
+  async function fetchCharacters() {
+    try {
+      const response = await axios.get(
+        "https://rickandmortyapi.com/api/character"
+      );
+
+      setCharacters(response.data.results);
+      console.log(response.data.results);
+
+    } catch (error) {
+      console.log("Erro ao buscar personagens");
+    }
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Personagens Rick and Morty</Text>
+
+      <FlatList
+        data={characters}
+        keyExtractor={(item) => String(item.id)}
+        renderItem={({ item }) => (
+          <View style={styles.card}>
+            <Image
+              source={{ uri: item.image }}
+              style={styles.image}
+            />
+
+            <Text style={styles.name}>{item.name}</Text>
+
+            <Text>Espécie: {item.species}</Text>
+
+            <Text
+              style={[
+                styles.status,
+                {
+                  color:
+                    item.status === "Alive"
+                      ? "green"
+                      : item.status === "Dead"
+                      ? "red"
+                      : "gray",
+                },
+              ]}
+            >
+              Status: {item.status}
+            </Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +67,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+    marginTop: 40,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+
+  card: {
+    backgroundColor: "#DDD",
+    padding: 15,
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 10,
+    alignSelf: "center",
+    marginBottom: 10,
+  },
+
+  name: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+
+  status: {
+    marginTop: 5,
+    fontWeight: "bold",
   },
 });
